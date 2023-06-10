dba0846bfac3125176e803b84d0c81bdf3b22dea
sngwebs786 <sngwebs786@gmail.com>
2023-06-10 18:30:16 +0500

    linked chakra ui

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from "@chakra-ui/react";
 const Navbar = () => {
   return (
     <>
       <div>Navbar</div>
+      <Button colorScheme="blue">Button</Button>
+      <Slider aria-label="slider-ex-1" defaultValue={30}>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
     </>
   );
 };
